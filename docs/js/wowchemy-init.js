be372ed57baee406c8a3cf66eb85f1b1e9fb51f1
Alessandro Puccetti <ale.puccetti@gmail.com>
2020-12-29 17:36:39 +0100

    Publish website

--- docs/js/wowchemy-init.js
+++ docs/js/wowchemy-init.js
@@ -1,5 +1,5 @@
 (() => {
-  // ns-hugo:/var/folders/5x/yhy21zbx23j87mst9jq5n7gm0000gn/T/hugo_cache/modules/filecache/modules/pkg/mod/github.com/wowchemy/wowchemy-hugo-modules/wowchemy@v0.0.0-20201224144527-9cce661db8d7/assets/js/wowchemy-theming.js
+  // ns-hugo:/var/folders/5x/yhy21zbx23j87mst9jq5n7gm0000gn/T/hugo_cache/modules/filecache/modules/pkg/mod/github.com/wowchemy/wowchemy-hugo-modules/wowchemy@v0.0.0-20201228231801-219b05cbd2c4/assets/js/wowchemy-theming.js
   var body = document.body;
   function getThemeMode() {
     return parseInt(localStorage.getItem("wcTheme") || 2);
